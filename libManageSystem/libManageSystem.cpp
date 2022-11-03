b93f0191c58f22836479523b1f87d2793799f3b2
文韬 居 <1985055836@qq.com>
2022-11-03 22:25:16 +0800

    全员就位开始

--- libManageSystem/libManageSystem.cpp
+++ libManageSystem/libManageSystem.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-    cout << "collaboration begins!!!" << endl;
+    cout << "collaboration begins..." << endl;
+    cout << "Ding Ruogu in position!" << endl;
+    cout << "Ju Wentao in position!" << endl;
 }
 
 
