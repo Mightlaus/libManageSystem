c7d8d2b4c65ed7e03c828901b7c2505e774e7127
Wentao <102165248+juwentao@users.noreply.github.com>
2022-11-03 21:23:45 +0800

    Update libManageSystem.cpp

--- libManageSystem/libManageSystem.cpp
+++ libManageSystem/libManageSystem.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Hello World!\n,this is a change by tao";
     cout << "collaboration begins" << endl;
     cout << "Wentao in position!" << endl;
 }
