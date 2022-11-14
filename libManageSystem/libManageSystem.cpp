a63905ab98d5548aa47dcdc7c2e9c658cb40157c
文韬 居 <1985055836@qq.com>
2022-11-14 12:42:18 +0800

    menu

--- libManageSystem/libManageSystem.cpp
+++ libManageSystem/libManageSystem.cpp
@@ -1,8 +1,49 @@
-﻿#include <iostream>
-
-using namespace std;
+﻿#include "head.h"
 
 int main()
 {
-	
+	cout << "********************菜单********************" << endl;
+	cout << "1.管理员模式" << endl;
+	cout << "2.用户模式" << endl;
+	cout << "3.退出" << endl;
+	while (1)
+	{
+		string option;
+		cin >> option;
+		if (option == "1")
+		{
+			cout << "请输入您的管理员账号和密码（默认账号为学号/教师编号，默认密码为123456）：" 
+				 << endl << "账号：";
+			string account;
+			cin >> account;
+			cout << "密码：";
+			string password;
+			cin >> password;
+
+
+			cout << "欢迎您！管理员" << account << endl;
+		}
+		else if (option == "2")
+		{
+			cout << "请输入您的用户账号和密码（默认账号为学号/教师编号，默认密码为123456）："
+				 << endl << "账号：";
+			string account;
+			cin >> account;
+			cout << "密码：";
+			string password;
+			cin >> password;
+
+
+			cout << "欢迎您！用户" << account << endl;
+		}
+		else if (option == "3")
+		{
+			return 0;
+		}
+		else
+		{
+			cout << "输入格式非法，请重试！"<<endl;
+		}
+	}
+
 }
