939eb21fd59b7b6265f2de89e27a750468229d54
文韬 居 <1985055836@qq.com>
2022-11-15 17:35:20 +0800

    出错后合并

--- libManageSystem/read_data.h
+++ libManageSystem/read_data.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
+#include <vector>
 using namespace std;
-void readcsv();
+void read_csv();
+void write_csv(int n, int m, string* s);
