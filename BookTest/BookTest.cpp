60748907cfbfb9d306334c362475b6a6faa51769
Ding Ruogu <78856759+Mightlaus@users.noreply.github.com>
2022-11-15 11:45:09 +0800

    book类基本框架完成，还没debug

---
+++ BookTest/BookTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#inclue "../libManage"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BookTest
+{
+	TEST_CLASS(BookTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
