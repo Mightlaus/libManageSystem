84d0d267d02521f2b7eff22d30f51e3756e5bb84
Ding Ruogu <78856759+Mightlaus@users.noreply.github.com>
2022-11-15 15:53:26 +0800

    book类属性加全，可以正常跑

--- libManageSystem/Book.h
+++ libManageSystem/Book.h
@@ -6,9 +6,6 @@ using namespace std;
 
 class Book
 {
-	Book(string caption, string author, unsigned long long isbn)
-		:caption(caption), author(author), isbn(isbn) {}
-
 private:
 	struct BorrowHistory
 	{
@@ -21,16 +18,46 @@ private:
 
 
 public:
+	Book(string caption, string author, unsigned long long isbn, string publishing, string published_time, int price)
+		:caption(caption), author(author), isbn(isbn), publishing(publishing), published_time(published_time), price(price) {}
+
+	Book(char* caption, char* author, unsigned long long isbn, char* publishing, char* published_time, int price)
+		:caption(caption), author(author), isbn(isbn), price(price)
+	{
+		string cp(caption);
+		this->caption = cp;
+		string au(author);
+		this->author = au;
+		string pb(publishing);
+		this->publishing = pb;
+		string pt(published_time);
+		this->published_time = pt;
+	}
+
+	// 属性
 	string caption;
 	string author;
 	unsigned long long isbn;
+	string publishing;
+	string published_time;
+	int price;
+
+
 	vector<BorrowHistory*> borrow_histories; // 里面存放的是结构体指针
 
+	//方法
 	void resetCaption(string new_caption);
+	void resetCaption(char* new_caption);
+
 	void resetAuthor(string new_author);
+	void resetAuthor(char* new_author);
+
 	void resetIsbn(unsigned long long new_isbn);
 
+	void resetPrice(int price);
+
 	void addBorrowHistory(int borrow_time = 0, string user_name = "default");
+	void addBorrowHistory(int borrow_time, char* user_name);
 
 };
 
--- libManageSystem/libManageSystem.cpp
+++ libManageSystem/libManageSystem.cpp
@@ -1,50 +1,8 @@
-﻿#include "head.h"
-
+﻿#include "Book.h"
 int main()
 {
-	cout << "********************菜单********************" << endl;
-	cout << "1.管理员模式" << endl;
-	cout << "2.用户模式" << endl;
-	cout << "3.退出" << endl;
-	while (1)
-	{
-		cout << "请输入您的选择：";
-		string option;
-		cin >> option;
-		if (option == "1")
-		{
-			cout << "请输入您的管理员账号和密码（默认账号为学号/教师编号，默认密码为123456）："
-				<< endl << "账号：";
-			string account;
-			cin >> account;
-			cout << "密码：";
-			string password;
-			cin >> password;
-
-
-			cout << "欢迎您！管理员" << account << endl;
-		}
-		else if (option == "2")
-		{
-			cout << "请输入您的用户账号和密码（默认账号为学号/教师编号，默认密码为123456）："
-				<< endl << "账号：";
-			string account;
-			cin >> account;
-			cout << "密码：";
-			string password;
-			cin >> password;
-
-
-			cout << "欢迎您！用户" << account << endl;
-		}
-		else if (option == "3")
-		{
-			return 0;
-		}
-		else
-		{
-			cout << "输入格式非法，请重试！" << endl;
-		}
-	}
-
+	string name = "str";
+	Book mybook("ap", "au", 998, "5waf", "2002-1", 10);
+	mybook.resetAuthor("haha");
+	mybook.addBorrowHistory(1, "okk");
 }
